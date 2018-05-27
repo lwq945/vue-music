5cd1163e71d2901bd7ce639039cb7f9043c218c6
lwq945 <lwq945@icloud.com>
2018-05-27 14:20:25 +0800

    add m-tab

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="app">
     <m-header></m-header>
+    <m-tab></m-tab>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 import MHeader from 'components/m-header/m-header.vue'
+import MTab from 'components/tab/tab.vue'
 export default {
   name: 'App',
   components: {
-    MHeader
+    MHeader,
+    MTab
   }
 }
 </script>
