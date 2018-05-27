0cd9f4507622d2f365398e0db38404f4750bd3f1
lwq945 <lwq945@icloud.com>
2018-05-27 11:23:03 +0800

    init projectX3

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App'
+import fastclick from 'fastclick'
+import 'babel-polyfill'
+import 'common/stylus/index.styl'
 
+fastclick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
