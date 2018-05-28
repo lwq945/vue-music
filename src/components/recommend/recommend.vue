f2cef2bb1f7824a0ea2a0e6e8078e2de4d46eb26
lwq945 <lwq945@icloud.com>
2018-05-28 16:42:59 +0800

    后台代理请求获取歌单的数据，并渲染

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -6,7 +6,17 @@
       </div>
       <div class="recommend-list">
         <h1 class="list-title">热门歌单推荐</h1>
-        <ul></ul>
+        <ul>
+          <li v-for="item in discList" :key="item.dissid" class="item">
+            <div class="icon">
+              <img class="icon-img" :src="item.imgurl">
+            </div>
+            <div class="text">
+              <h2 class="name" v-html="item.creator.name"></h2>
+              <p class="desc" v-html="item.dissname"></p>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -14,23 +24,32 @@
 
 <script>
 import BaseSlider from 'base/slider/slider.vue'
-import getRecommend from 'api/recommend.js'
+import { getRecommend, getDiscList } from 'api/recommend.js'
 import {ERR_OK} from 'api/config.js'
 export default {
   name: 'Recommend',
   data() {
     return {
-      recommendSliders: []
+      recommendSliders: [],
+      discList: []
     }
   },
   created() {
     this._getRecommend()
+    this._getDiscList()
   },
   methods: {
     _getRecommend() {
       getRecommend().then((res) => {
         if (res.code === ERR_OK) {
           this.recommendSliders = res.data.slider
+        }
+      })
+    },
+    _getDiscList() {
+      getDiscList().then((res) => {
+        if (res.code === ERR_OK) {
+          this.discList = res.data.list
         }
       })
     }
@@ -44,11 +63,51 @@ export default {
 <style lang="stylus" scoped>
   @import "~common/stylus/variable"
   .recommend
-    position: absolute
+    position: fixed
+    width: 100%
     top: 88px
     bottom: 0
-    width: 100%
     .recommend-content
       height: 100%
       overflow: hidden
+      .slider-wrapper
+        position: relative
+        width: 100%
+        overflow: hidden
+      .recommend-list
+        .list-title
+          height: 65px
+          line-height: 65px
+          text-align: center
+          font-size: $font-size-medium
+          color: $color-theme
+        .item
+          display: flex
+          box-sizing: border-box
+          align-items: center
+          padding: 0 20px 20px 20px
+          .icon
+            flex: 0 0 60px
+            width: 60px
+            padding-right: 20px
+            .icon-img
+              width: 100%
+          .text
+            display: flex
+            flex-direction: column
+            justify-content: center
+            flex: 1
+            line-height: 20px
+            overflow: hidden
+            font-size: $font-size-medium
+            .name
+              margin-bottom: 10px
+              color: $color-text
+            .desc
+              color: $color-text-d
+      .loading-container
+        position: absolute
+        width: 100%
+        top: 50%
+        transform: translateY(-50%)
 </style>
