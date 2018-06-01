3da8628e3222c1ae989df970a29407a8d7c39e08
lwq945 <lwq945@icloud.com>
2018-06-01 21:41:33 +0800

    添加组件的效果

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
+      <div class="play-wrapper">
+        <div class="play" v-show="songs.length>0" ref="playBtn">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
       <div class="filter"></div>
     </div>
     <div class="bg-layer" ref="layer"></div>
@@ -17,6 +23,9 @@
       <div class="song-list-wrapper">
         <song-list :songs="songs"></song-list>
       </div>
+      <div v-show="!songs.length" class="loading-container">
+        <base-loading></base-loading>
+      </div>
     </base-scroll>
   </div>
 </template>
@@ -24,6 +33,7 @@
 <script>
 import BaseScroll from 'base/scroll/scroll.vue'
 import SongList from 'base/song-list/song-list.vue'
+import BaseLoading from 'base/loading/loading'
 const RESERVED_HEIGHT = 40
 export default {
   props: {
@@ -65,19 +75,43 @@ export default {
     scroll(pos) {
       this.scrollY = pos.y
       // console.log('scrollY:' + this.scrollY)
+    },
+    back() {
+      this.$router.back()
     }
   },
   watch: {
     scrollY(newY) {
       let translateY = Math.max(this.minTranslateY, newY)
       // console.log('translateY:' + translateY)
+      let zIndex = 0
+      let scale = 1
       this.$refs.layer.style['transform'] = `translate3d(0,${translateY}px,0)`
       this.$refs.layer.style['webkitTransform'] = `translate3d(0,${translateY}px,0)`
+      const percent = Math.abs(newY / this.imageHeight)
+      if (newY > 0) {
+        scale = 1 + percent
+        zIndex = 10
+      }
+      if (newY < this.minTranslateY) {
+        zIndex = 10
+        this.$refs.bgImage.style.paddingTop = 0
+        this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+        this.$refs.playBtn.style.display = 'none'
+      } else {
+        this.$refs.bgImage.style.paddingTop = `70%`
+        this.$refs.bgImage.style.height = 0
+        this.$refs.playBtn.style.display = ''
+      }
+      this.$refs.bgImage.style.zIndex = zIndex
+      this.$refs.bgImage.style['transform'] = `scale(${scale})`
+      this.$refs.bgImage.style['webkitTransform'] = `scale(${scale})`
     }
   },
   components: {
     BaseScroll,
-    SongList
+    SongList,
+    BaseLoading
   }
 }
 </script>
