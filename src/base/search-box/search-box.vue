d0646a9492ca8b227555e0f2391edc0fe2d295ef
lwq945 <lwq945@icloud.com>
2018-06-04 15:27:22 +0800

    添加search-box组件的样式布局和事件配置

---
+++ src/base/search-box/search-box.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="search-box">
+    <i class="icon-search"></i>
+    <input type="text" class="box" v-model="queryWord" :placeholder="placeholder">
+    <i class="icon-dismiss" v-show="queryWord" @click="clear"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    placeholder: {
+      type: String,
+      default: '搜索歌曲、歌手'
+    }
+  },
+  data() {
+    return {
+      queryWord: ''
+    }
+  },
+  created() {
+    this.$watch('query', (newquery) => {
+      this.$emit('query', newquery)
+    })
+  },
+  methods: {
+    clear() {
+      this.queryWord = ''
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~common/stylus/variable"
+
+  .search-box
+    display: flex
+    align-items: center
+    width: 100%
+    height: 40px
+    padding: 0 6px
+    box-sizing: border-box
+    background: $color-highlight-background
+    border-radius: 6px
+    .icon-search
+      font-size: 24px
+      color: $color-background
+    .box
+      flex: 1
+      margin: 0 5px
+      line-height: 18px
+      background: $color-highlight-background
+      color: $color-text
+      font-size: $font-size-medium
+      outline: none
+      &::placeholder
+        color: $color-text-d
+    .icon-dismiss
+      font-size: 16px
+      color: $color-background
+</style>
--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,13 +1,27 @@
 <template>
-  <div>
-    search
+  <div class="search">
+    <div class="search-box-wrapper">
+      <search-box></search-box>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+import SearchBox from 'base/search-box/search-box.vue'
+
+export default {
+  components: {
+    SearchBox
+  }
+}
 </script>
 
-<style>
-
+<style lang="stylus" scoped>
+  .search
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
+    .search-box-wrapper
+      margin: 20px
 </style>
